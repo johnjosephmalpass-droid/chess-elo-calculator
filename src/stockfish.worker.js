066910a30a9c9813d05b67c47c59050350ecf695
johnjosephmalpass-droid <johnjosephmalpass@gmail.com>
2026-02-10 16:09:20 +0000

    Update Stockfish script import to version 0.10.0

--- src/stockfish.worker.js
+++ src/stockfish.worker.js
@@ -3,9 +3,7 @@
 postMessage("info string Worker booted");
 
 try {
-  importScripts(
-    "https://cdn.jsdelivr.net/npm/stockfish@16.0.0/src/stockfish-16.1-lite-single.js"
-  );
+  importScripts("https://cdn.jsdelivr.net/npm/stockfish.js@0.10.0/stockfish.js");
   postMessage("info string Stockfish loaded");
 } catch (e) {
   postMessage("info string Failed to load Stockfish");
